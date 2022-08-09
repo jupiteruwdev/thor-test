bece85a31d00f303bdc6ffd1f273c5591b38cfa6
jupiteruwdev <harutoinoue8@gmail.com>
2022-08-08 21:56:20 -0400

    front end updated.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
+import { UseWalletProvider } from 'use-wallet'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UseWalletProvider chainId={1}>
+      <Component {...pageProps} />
+    </UseWalletProvider>
+  )
 }
 
 export default MyApp
